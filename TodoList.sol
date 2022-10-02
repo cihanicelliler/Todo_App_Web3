e5e1818dc51109f9db86dde5d9aa26288e65410a
Cihan <icellilercihan@gmail.com>
2022-10-02 22:06:52 +0300

    Project has been changed

--- TodoList.sol
+++ TodoList.sol
@@ -1,48 +1,38 @@
+pragma solidity ^0.8.7;
 
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
-
-contract Todos {
+// Insert, update, read from array of structs
+contract TodoList {
     struct Todo {
         string text;
         bool completed;
     }
-
-    // An array of 'Todo' structs
+    // we are declaring an array of the struct
     Todo[] public todos;
 
-    function create(string calldata _text) public {
-        // 3 ways to initialize a struct
-        // - calling it like a function
-        todos.push(Todo(_text, false));
-
-        // key value mapping
-        todos.push(Todo({text: _text, completed: false}));
-
-        // initialize an empty struct and then update it
-        Todo memory todo;
-        todo.text = _text;
-        // todo.completed initialized to false
-
-        todos.push(todo);
+    // creating a todo array
+    function create(string calldata _text) external {
+        // pushing _text into the todo array
+        todos.push(Todo({
+            text: _text,
+            completed: false
+        }));
     }
 
-    // Solidity automatically created a getter for 'todos' so
-    // you don't actually need this function.
-    function get(uint _index) public view returns (string memory text, bool completed) {
+    // updating description of the given index of the array
+    function updateText(uint _index, string calldata _text) external {
         Todo storage todo = todos[_index];
+        todo.text = _text;
+    }
+
+    function get(uint _index) external view returns(string memory, bool) {
+        // storage - 29397 gas --- cheaper option
+        // memory - 29480 gas
+        Todo memory todo = todos[_index];
         return (todo.text, todo.completed);
     }
 
-    // update text
-    function updateText(uint _index, string calldata _text) public {
-        Todo storage todo = todos[_index];
-        todo.text = _text;
-    }
-
-    // update completed
-    function toggleCompleted(uint _index) public {
-        Todo storage todo = todos[_index];
-        todo.completed = !todo.completed;
+    // marking as completed/incompleted function
+    function toggleCompleted(uint _index) external {
+        todos[_index].completed = !todos[_index].completed;
     }
 }
